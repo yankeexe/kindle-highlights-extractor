b93e4b4589eb2500d5b2631167f06d0e84a7187d
yankeexe <yankee.exe@gmail.com>
2020-08-27 08:23:10 +0545

    Simple Web UI to for file upload

--- kindle_extractor/app.py
+++ kindle_extractor/app.py
@@ -3,7 +3,7 @@ from itertools import zip_longest, islice
 from collections import defaultdict
 
 from werkzeug.utils import secure_filename
-from flask import Flask, abort, request, send_file
+from flask import Flask, abort, request, send_file, render_template, redirect
 
 from kindle_extractor import config, helpers
 
@@ -13,8 +13,12 @@ def create_app():
     Application factory entrypoint.
     """
 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates")
     app.config.from_object(config.Config)
+
+    @app.route("/app")
+    def homepage():
+        return render_template("base.html")
 
     @app.route("/jsonify", methods=["POST"])
     def clippings_jsonify():
@@ -25,12 +29,12 @@ def create_app():
         input_tempfile = helpers.temp_file(".txt")
 
         if not request.files:
-            abort(406, "Request payload can only be files")
+            abort(406, "Request payload can only be files.")
 
-        file = request.files['file']
+        file = request.files["clippings"]
         print(request.files)
         filename = secure_filename(file.filename)
-        print('secure filename', filename)
+        print("secure filename", filename)
 
         if not filename:
             abort(404, "File not found.")
@@ -60,9 +64,11 @@ def create_app():
         for key, value in data_dict.items():
             data_dict[key] = list(set(value))
 
+        return redirect()
+
         # Output JSON file.
         output_tempfile = helpers.temp_file(".json", delete=False)
-        print('output tempfile', output_tempfile)
+        print("output tempfile", output_tempfile)
 
         with open(output_tempfile, "w+") as f:
             json.dump(dict(data_dict), f, indent=4)
